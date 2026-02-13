ac6129f32ca36dcc6fd9280bbb84c7cbefa6f5b9
mahdi839 <hasanarefi56574@gmail.com>
2026-02-14 00:20:59 +0600

    added gsap animations

---
+++ index.js
@@ -0,0 +1,31 @@
+ gsap.from("header", {
+        x: 200,
+        opacity: 0,
+        duration: 3,
+        delay:1
+    });
+
+    gsap.from("h1", {
+        y: 60,
+        opacity: 0,
+        duration: 2,
+        delay: 1,
+        stagger:true
+    });
+
+    gsap.from("p", {
+        y: 40,
+        opacity: 0,
+        duration: 1,
+        delay: 0.6,
+        ease: "power2.out"
+    });
+
+    gsap.from("main button", {
+        scale: 0.8,
+        opacity: 0,
+        duration: 0.8,
+        delay: 0.9,
+        ease: "back.out(1.7)"
+    });
+
