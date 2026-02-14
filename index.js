c39fca1cc7998a631289acb9a7dc815e68597ae7
mahdi839 <hasanarefi56574@gmail.com>
2026-02-14 20:18:47 +0600

    add animation on why choose section cards

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+gsap.registerPlugin(ScrollTrigger);
 gsap.from("header", {
     y: -200,
     opacity: 0,
@@ -16,7 +16,7 @@ gsap.from("h1", {
 });
 
 gsap.from(".for_rent_header h2, .for_rent_header p", {
-    y: 40,
+    x: -400,
     opacity: 0,
     duration: 1,
     stagger: 0.25,
@@ -28,7 +28,46 @@ gsap.from(".for_rent_header h2, .for_rent_header p", {
     }
 });
 
+// Feature 1 - from left
+gsap.from('#feature_1', {
+    x: -200,
+    opacity: 0,
+    duration: 2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: '#why_choos_section',
+        start: "top 70%",
+        toggleActions: "play reverse play reverse"
+    }
+});
 
+// Feature 2 - from top
+gsap.from('#feature_2', {
+    y: -200,
+    opacity: 0,
+    duration: 2,
+    delay: 0.2,  // Slight delay for stagger effect
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: '#why_choos_section',
+        start: "top 70%",
+        toggleActions: "play reverse play reverse"
+    }
+});
+
+// Feature 3 - from right
+gsap.from('#feature_3', {
+    x: 200,
+    opacity: 0,
+    duration: 2,
+    delay: 0.4,  // More delay for third card
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: '#why_choos_section',
+        start: "top 70%",
+        toggleActions: "play reverse play reverse"
+    }
+});
 gsap.from("#image-div", {
     x: 500,
     duration: 2,
